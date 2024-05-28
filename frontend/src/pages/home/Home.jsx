063cfd5e03a895bcb128939878d5b77f4471eab8
Saurabh Nagar <68803906+sk790@users.noreply.github.com>
2024-05-28 21:15:23 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import MessagesContainer from "../../components/messages/MessagesContainer";
 const Home = () => {
   return (
     <div className="w-full h-full flex rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-      <div className="hidden md:block sm:hidden w-1/2">
+      <div className="w-1/2">
         <Sidebar />
       </div>
       <MessagesContainer />
